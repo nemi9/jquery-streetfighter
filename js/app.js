c5d1c4b56cd1d0944f1d6268ca6f7f3c51907ff1
Jason Turner <yourstageyourmusic@gmail.com>
2014-06-16 19:38:34 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
 				{'margin-left': '500px'},
 				800,
 				function() {
+					$.alert(1)
 					$(this).hide();
 					$(this).css('margin-left', '-10px');
 							} 
